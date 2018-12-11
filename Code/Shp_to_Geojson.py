0e8bc4a050263367c83cabb4ea411c0079bd6457
Sachin Sharma <sachinsharma9780@gmail.com>
2018-12-11 21:01:59 +0100

    Update Shp_to_Geojson.py

--- Code/Shp_to_Geojson.py
+++ Code/Shp_to_Geojson.py
@@ -15,12 +15,7 @@ def shp_to_GeoJson(path):
         print(r'ogr2ogr -f GeoJSON {}.geojson {}.shp'.format(basename, basename))
         subprocess.call(r'ogr2ogr -f GeoJSON -t_srs EPSG:4326 -s_srs EPSG:3035 {}.geojson {}.shp '.format(basename, basename))
 
-# Visualizing Kml file on Google Earth
-def visulaize_kml(path):
-    for file in os.listdir(path):
-        basename_kml, ext = os.path.splitext(file)
-        os.startfile("{}.kml".format(basename_kml))
-          
 
+# Calling Function          
 shp_to_GeoJson(r"C:\Users\Sachin Sharma\Desktop\CollaborativeIntelligence_Project\Test")     
-visulaize_kml(r"C:\Users\Sachin Sharma\Desktop\CollaborativeIntelligence_Project\KMl_files")   
+  
