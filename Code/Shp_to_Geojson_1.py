0413cfbcb478d14da705a40e270d2edd61f27a2e
Sachin Sharma <sachinsharma9780@gmail.com>
2018-12-11 21:17:00 +0100

    ShpFile_to _Geojson_conversion

---
+++ Code/Shp_to_Geojson_1.py
@@ -0,0 +1,21 @@
+# Importing necessrary Libraries
+import gdal
+import cv2
+import subprocess
+import os
+import subprocess
+import webbrowser
+import glob
+
+# Converting shp file to kml
+def shp_to_GeoJson(path):
+    os.chdir(path)
+    for file in glob.glob("*.shp"):
+        basename, ext = os.path.splitext(file)
+        print(r'ogr2ogr -f GeoJSON {}.geojson {}.shp'.format(basename, basename))
+        subprocess.call(r'ogr2ogr -f GeoJSON -t_srs EPSG:4326 -s_srs EPSG:3035 {}.geojson {}.shp '.format(basename, basename))
+
+
+# Calling Function          
+shp_to_GeoJson(r"C:\Users\Sachin Sharma\Desktop\CollaborativeIntelligence_Project\Test")     
+  
